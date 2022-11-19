cc57c8f8bfc3a11ac1ea2f8087f44027b343f6bd
Ismail <i180516@nu.edu.pk>
2022-11-20 00:22:08 +0500

    Changed spinner style File Name - minor changing.

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,6 +1,4 @@
-import { SpinnerContainer, SpinnerOverlay } from './spinnerStyles'
-
-import React from 'react'
+import { SpinnerContainer, SpinnerOverlay } from './spinner.styles'
 
 const Spinner = () => {
   return (
